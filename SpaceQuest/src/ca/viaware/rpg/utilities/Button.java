27efa500df49714fa4c5d7634320fdaa502531eb
Seth T. <seth@viaware.ca>
2012-12-14 17:30:49 -0700

    Made button class use AbstractEntity
    Just so you dont forget

---
+++ SpaceQuest/src/ca/viaware/rpg/utilities/Button.java
@@ -0,0 +1,29 @@
+package ca.viaware.rpg.utilities;
+
+import ca.viaware.rpg.entity.AbstractEntity;
+
+public class Button extends AbstractEntity{
+	
+	public Button(double x, double y, double width, double height) {
+		super(x, y, width, height);
+	}
+
+	private TexturedQuad Button;
+	
+	
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(int delta) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
