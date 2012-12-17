2e192af3ce31982a0b87d995107945483bfc463e
Seth T <seth@viaware.ca>
2012-12-17 16:55:13 -0700

    Rename part 1

--- SpaceQuest/src/ca/viaware/rpg/entities/AmmoBar2.java
+++ SpaceQuest/src/ca/viaware/rpg/entities/AmmoBar2.java
@@ -7,12 +7,12 @@ import ca.viaware.rpg.utilities.TexturedQuad;
 
 
 
-public class AmmoBar{
+public class AmmoBar2{
 	TextRenderer tRender = new TextRenderer(30,0,TextRenderer.Font.BLACK);
 	
 	TexturedQuad back;
 	
-	public AmmoBar(){
+	public AmmoBar2(){
 		
 		back = new TexturedQuad(65,65,Display.getWidth()-60,Display.getHeight()-120,180,"res/sprites/other/Ammo Icon.png");
 		
--- SpaceQuest/src/ca/viaware/rpg/game/GameLoop.java
+++ SpaceQuest/src/ca/viaware/rpg/game/GameLoop.java
@@ -15,7 +15,7 @@ import ca.viaware.rpg.entities.HealthBar;
 import ca.viaware.rpg.entities.Player;
 import ca.viaware.rpg.entities.Slime;
 import ca.viaware.rpg.entities.WeakSlime;
-import ca.viaware.rpg.entities.AmmoBar;
+import ca.viaware.rpg.entities.AmmoBar2;
 import ca.viaware.rpg.map.Loader;
 import ca.viaware.rpg.map.Map;
 import ca.viaware.rpg.map.MapHandler;
@@ -42,7 +42,7 @@ public class GameLoop {
 				//Globals.enemies.add(new FastSlime(10,10));
 				//Globals.enemies.add(new WeakSlime(30,30));
 
-		Globals.ammoBar=new AmmoBar();
+		Globals.ammoBar=new AmmoBar2();
 		
 		Globals.gameMap = MapHandler.handleMapLoad("TestMap1");
 		MapHandler.finishLoad();
