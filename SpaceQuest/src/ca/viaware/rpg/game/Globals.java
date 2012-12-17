197165f09fc514e9122717b4bb5729091b8d9165
Seth T <seth@viaware.ca>
2012-12-17 16:55:56 -0700

    Rename part 2

--- SpaceQuest/src/ca/viaware/rpg/game/Globals.java
+++ SpaceQuest/src/ca/viaware/rpg/game/Globals.java
@@ -13,7 +13,7 @@ import ca.viaware.rpg.entities.HealthBar;
 import ca.viaware.rpg.entities.Player;
 import ca.viaware.rpg.entities.Slime;
 import ca.viaware.rpg.entities.WeakSlime;
-import ca.viaware.rpg.entities.AmmoBar2;
+import ca.viaware.rpg.entities.AmmoBar;
 import ca.viaware.rpg.map.Map;
 import ca.viaware.rpg.map.TeleMarker;
 import ca.viaware.rpg.utilities.Button;
@@ -26,7 +26,7 @@ public class Globals {
 	public static HealthBar healthBar;
 	//please note mobs should not be spawned at 0,0 or where they cannot reach player
 	public static ArrayList <Enemy> enemies=new ArrayList<Enemy>();
-	public static AmmoBar2 ammoBar;
+	public static AmmoBar ammoBar;
 	public static boolean isRunning = true;
 	public static int dispWidth = 800, dispHeight = 600;
 	public static int dispWidthBK = 0, dispHeightBK = 0;
