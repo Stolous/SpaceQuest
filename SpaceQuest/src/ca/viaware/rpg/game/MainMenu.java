481388d1c35b1e5c988c20dc65d2d3cf67973c70
Seth T <seth@viaware.ca>
2012-12-18 20:35:25 -0700

    Rolled back MainMenu to pre-merge

--- SpaceQuest/src/ca/viaware/rpg/game/MainMenu.java
+++ SpaceQuest/src/ca/viaware/rpg/game/MainMenu.java
@@ -6,6 +6,7 @@ import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glTexCoord2f;
 import static org.lwjgl.opengl.GL11.glVertex2i;
 
+import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.opengl.Texture;
 
 import ca.viaware.rpg.game.Globals.gState;
@@ -15,12 +16,14 @@ import ca.viaware.rpg.utilities.SButton;
 import ca.viaware.rpg.utilities.TextureHandler;
 import ca.viaware.rpg.utilities.TexturedQuad;
 
-public class MainMenu {
+public class MainMenu { 
+	private double UFOtrans  =1; 
 	private int movementstage = 0;
 	Texture [] backGround = new Texture [3];
 	SButton start,options,help,quit;
 	TexturedQuad UFO;
 	private int location=0;
+	private boolean onethread = false;
 public MainMenu(){
 	TextureHandler t = new TextureHandler();
 	String s = "other/main menu screen000";
@@ -67,7 +70,9 @@ public MainMenu(){
 		if(location== 3){
 			location=0;
 		}
+		
 		glBegin(GL_QUADS);
+		
 		glTexCoord2f(0f, 0f);
 		glVertex2i(0, 0);
 		glTexCoord2f(1f, 0f);
@@ -77,6 +82,7 @@ public MainMenu(){
 		glTexCoord2f(0f, 1f);
 		glVertex2i(0, Globals.dispHeight);
 		glEnd();
+		GL11.glColor4d(1, 1, 1, 1);
 		quit.updateOnClickListener(new OnClickListener(quit));
 		quit.AddOnClickListener(new ClickListener() {
 			
@@ -96,13 +102,12 @@ public MainMenu(){
 				
 				Runnable r = new Runnable() {
 					public void run() {
-						try {
-							Thread.sleep(50L);// this is so that the color shows
-												// for a little while
+						if(onethread==false){
+						onethread =true;
+							
 							System.out.println("Switching Gamestate to game");
-							Globals.gameState = Globals.gState.GAME;
-						} catch (InterruptedException e) {
-							e.printStackTrace();
+							transition(Globals.gState.GAME);
+							System.out.println("YAY");
 						}
 
 					}
@@ -119,7 +124,54 @@ public MainMenu(){
 		Globals.buttonBuffer.add(help);
 		Globals.buttonBuffer.add(start);
 		Globals.buttonBuffer.add(options);
+		UFO.settrans(UFOtrans);
 		UFO.update();
 	
+	}
+	
+	private void transition(final gState g){
+		
+		Runnable r = new Runnable(){
+			private volatile boolean stopRequested = false;
+			@Override
+			public void run() {
+				while(!stopRequested){
+				for(int i =0; i<450;i++){
+					
+					start.move(-0.25, 0);
+					quit.move(-0.25, 0);
+					options.move(-0.25, 0);
+					help.move(-0.25, 0);
+					UFOtrans -=0.001;
+					
+					
+					try {
+						Thread.sleep(2);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if(start.getX()<-350){
+						
+						Globals.gameState=g;
+						stops();
+					}
+					
+					
+				}
+			}
+				}
+			public void stops(){
+				System.out.println("Stopped");
+				stopRequested = true;
+			}
+			
+		};
+		Thread t = new Thread(r);
+		t.start();
+		
+		
+		
+		
 	}
 }
