ecb240ecdbac336a0fc0275bd8f26863b8949a39
Seth T <seth@viaware.ca>
2012-12-21 17:52:46 -0700

    Moved texture loading to another class

--- SpaceQuest/src/ca/viaware/rpg/game/GameLoop.java
+++ SpaceQuest/src/ca/viaware/rpg/game/GameLoop.java
@@ -43,7 +43,7 @@ public class GameLoop {
 		Globals.playerEntity = new Player(Globals.dispWidth / 2 - 32, Globals.dispHeight / 2 - 32, 64, 64);
 		Globals.cursor = new Cursor(0, 0, 32, 32);
 		Globals.cursor.setEnabled(true);
-		setupTextures();
+		TextureLoadList.loadTextures();
 		Renderer render = new Renderer();
 		GameLogic logic = new GameLogic();
 		DebugScreen dbs = new DebugScreen(170, 200);
@@ -83,51 +83,6 @@ public class GameLoop {
 		}
 		Display.destroy();
 		System.exit(0);
-	}
-
-	private void setupTextures() {
-		TextureHandler th = new TextureHandler();
-		Globals.otherTextures.add(th.loadDiffTexture("res/img/intro2.png", "PNG"));
-		Globals.otherTextures.add(th.loadDiffTexture("res/img/maploading.png", "PNG"));
-		
-		Globals.tileTextures.add(th.loadSprite("tiles/grass"));
-		Globals.tileTextures.add(th.loadSprite("tiles/cobble"));
-		Globals.tileTextures.add(th.loadSprite("tiles/flower red"));
-		Globals.tileTextures.add(th.loadSprite("tiles/flower"));
-		Globals.tileTextures.add(th.loadSprite("tiles/palm"));
-		Globals.tileTextures.add(th.loadSprite("tiles/sand"));
-		Globals.tileTextures.add(th.loadSprite("tiles/tree"));
-		Globals.tileTextures.add(th.loadSprite("tiles/alien cactus plant 1"));
-		Globals.tileTextures.add(th.loadSprite("tiles/alien cactus plant 2"));
-		Globals.tileTextures.add(th.loadSprite("tiles/double alien cactus plant"));
-		Globals.tileTextures.add(th.loadSprite("tiles/red rock"));
-		Globals.tileTextures.add(th.loadSprite("tiles/sign"));
-		Globals.tileTextures.add(th.loadSprite("tiles/dirt"));
-
-		System.out.println("Loaded " + Globals.tileTextures.size() + " tile textures");
-
-		// Player animation stages - Temporary, messy and needs redoing
-		Globals.playerEntity.addTexture(th.loadSprite("player/down/1"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/down/2"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/down/3"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/down/4"));
-
-		Globals.playerEntity.addTexture(th.loadSprite("player/up/1"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/up/2"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/up/3"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/up/4"));
-
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/1"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/2"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/3"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/4"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/5"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/6"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/7"));
-		Globals.playerEntity.addTexture(th.loadSprite("player/right/8"));
-
-		Globals.cursor.setTexture(th.loadSprite("other/cursor"));
-
 	}
 
 	public int getDelta() {
